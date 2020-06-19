9da01b1e22b3ea4ad7c1cd1a3f498ae86c34d82d
SCOTT-HAMILTON <sgn.hamilton@protonmail.com>
2020-06-19 23:25:25 +0200

    Pytests

---
+++ tests.py
@@ -0,0 +1,7 @@
+import pytest
+
+from MergeKeepass.keepassmerge import merge_databases
+
+def test_merge_databases():
+    assert not merge_databases([ 'testing/DB1.kdbx', 'testing/DB2.kdbx' ], 'testing/DB-out.kdbx', 'test')
+
